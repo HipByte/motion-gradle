819a5246060f161c5925a950e7f4ab6dfc0f7279
Petrik <petrik@deheus.net>
2022-03-23 11:17:38 +0100

    Update config for Gradle version 7
    The `--build-file` option has been replaced by `--project-root`.
    
    The `duplicatesStrategy` has to be explicitly set:
    https://docs.gradle.org/current/userguide/upgrading_version_5.html#implicit_duplicate_strategy_for_copy_or_archive_tasks_has_been_deprecated
    
    `compile` has been deprecated and should be replaced with `implementation`.

--- lib/motion/project/gradle.rb
+++ lib/motion/project/gradle.rb
@@ -78,7 +78,7 @@ module Motion::Project
       vendor_aidl_files
       generate_settings_file
       generate_build_file
-      system("#{gradle_command} --build-file #{build_file} generateDependencies")
+      system("#{gradle_command} --project-dir #{GRADLE_ROOT} generateDependencies")
 
       # this might be uneeded in the future
       # if RM does support .aar out of the box
